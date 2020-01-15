caa7d47cbfc760c44d17e619667ab6287eaae47c
Skyler H Gold <skylergold96@gmail.com>
2020-01-15 16:21:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,28 +1,7 @@
-//var playlist = new Object({artist1:"song"})
+var playlist = new Object({artist1:"song"})
 
-//function updatePlaylist (playlist,name,song){
- // playlist.name = "song";
- // return updatePlaylist
-//}
-
-var playlist = {
-  artist1: "song1",
-  artist2: "song2",
-  artist3: "song3"
-};
-
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+function updatePlaylist (playlist,name,song){
+  playlist.name = "song";
+  return updatePlaylist
 }
-
-updatePlaylist(playlist, "artist4", "song4");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
-}
-
-removeFromPlaylist(playlist, "artist2");
-
-
 
